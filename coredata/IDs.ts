5926960b057aa0e67b8412139f9bd38e77d41ed0
nullcst <skull_magician@hotmail.com>
2021-08-27 10:49:52 -0300

    dsfsd

---
+++ coredata/IDs.ts
@@ -0,0 +1,39 @@
+// @ts-nocheck
+// To use these IDs in your live scripts
+// simply copy the line into your script.
+export const TLRCLASSES_NECROMANCER : uint32 = GetID("ChrClasses","TLRClasses","necromancer");
+export const TLRCLASSES_SUMMONING_SKILL : uint32 = GetID("SkillLineAbility","TLRClasses","summoning-skill");
+export const TLRCLASSES_PLAGUE_SKILL : uint32 = GetID("SkillLineAbility","TLRClasses","plague-skill");
+export const TLRCLASSES_BONEWORKS_SKILL : uint32 = GetID("SkillLineAbility","TLRClasses","boneworks-skill");
+export const TLRCLASSES_ROGUE_TUMBLE : uint32 = GetID("Spell","TLRClasses","rogue-tumble");
+export const TLRHOUSING_HOUSING_SKILL : uint32 = GetID("SkillLineAbility","TLRHousing","housing-skill");
+export const TLRHOUSING_GMTEST_HS_TABLE_HUMAN : uint32 = GetID("gameobject_template","TLRHousing","gmtest-hs-table-human");
+export const TLRHOUSING_GMTEST_HS_BOTTLE_GREEN : uint32 = GetID("gameobject_template","TLRHousing","gmtest-hs-bottle-green");
+export const TLRHOUSING_GMTEST1_HS_TABLE_HUMAN : uint32 = GetID("gameobject_template","TLRHousing","gmtest1-hs-table-human");
+export const TLRHOUSING_GMTEST1_HS_BOTTLE_GREEN : uint32 = GetID("gameobject_template","TLRHousing","gmtest1-hs-bottle-green");
+export const TLRHOUSING_GMTEST_PLACE_HS_TABLE_HUMAN : uint32 = GetID("Spell","TLRHousing","gmtest-place-hs-table-human");
+export const TLRHOUSING_GMTEST_PLACE_HS_BOTTLE_GREEN : uint32 = GetID("Spell","TLRHousing","gmtest-place-hs-bottle-green");
+export const TLRHOUSING_GMTEST1_PLACE_HS_TABLE_HUMAN : uint32 = GetID("Spell","TLRHousing","gmtest1-place-hs-table-human");
+export const TLRHOUSING_GMTEST1_PLACE_HS_BOTTLE_GREEN : uint32 = GetID("Spell","TLRHousing","gmtest1-place-hs-bottle-green");
+export const TLRHOUSING_HOUSING_SPELL : uint32 = GetID("Spell","TLRHousing","housing-spell");
+export const TLRHOUSING_HS_BASE_GMONLY : uint32 = GetID("item_template","TLRHousing","hs-base-gmonly");
+export const TLRHOUSING_HS_GMONLY_HS_TABLE_HUMAN : uint32 = GetID("item_template","TLRHousing","hs-gmonly-hs-table-human");
+export const TLRHOUSING_HS_GMONLY_HS_BOTTLE_GREEN : uint32 = GetID("item_template","TLRHousing","hs-gmonly-hs-bottle-green");
+export const TLRHOUSING_GMTEST_PLACE_HS_TABLE_HUMAN : uint32 = GetID("item_template","TLRHousing","gmtest-place-hs-table-human");
+export const TLRHOUSING_GMTEST_PLACE_HS_BOTTLE_GREEN : uint32 = GetID("item_template","TLRHousing","gmtest-place-hs-bottle-green");
+export const TLRHOUSING_GMTEST1_PLACE_HS_TABLE_HUMAN : uint32 = GetID("item_template","TLRHousing","gmtest1-place-hs-table-human");
+export const TLRHOUSING_GMTEST1_PLACE_HS_BOTTLE_GREEN : uint32 = GetID("item_template","TLRHousing","gmtest1-place-hs-bottle-green");
+export const TLRCREATURES_WRATHION_QUEST : uint32 = GetID("creature_template","TLRCreatures","wrathion-quest");
+export const TLRCREATURES_WRATHION_QUESTSPAWN : uint32 = GetID("creature","TLRCreatures","wrathion-questspawn");
+export const TLREXPANSION_HS_BOTTLE_GREEN : uint32 = GetID("gameobject_template","TLRExpansion","hs-bottle-green");
+export const TLREXPANSION_HS_BOTTLE_GREEN1 : uint32 = GetID("gameobject_template","TLRExpansion","hs-bottle-green1");
+export const TLREXPANSION_HS_TABLE_HUMAN : uint32 = GetID("gameobject_template","TLRExpansion","hs-table-human");
+export const TLREXPANSION_GMTEST_HS_TABLE_HUMAN : uint32 = GetID("gameobject_template","TLRExpansion","gmtest-hs-table-human");
+export const TLREXPANSION_GMTEST_HS_BOTTLE_GREEN : uint32 = GetID("gameobject_template","TLRExpansion","gmtest-hs-bottle-green");
+export const TLREXPANSION_TEST : uint32 = GetID("gameobject","TLRExpansion","test");
+export const TLREXPANSION_HS_BOTTLE_GREEN : uint32 = GetID("gameobject","TLRExpansion","hs-bottle-green");
+export const TLREXPANSION_HS_BOTTLE_GREEN1 : uint32 = GetID("gameobject","TLRExpansion","hs-bottle-green1");
+export const TLREXPANSION_HS_TABLE_HUMAN : uint32 = GetID("gameobject","TLRExpansion","hs-table-human");
+export const TLREXPANSION_PLACE_OBJECT_TEST : uint32 = GetID("Spell","TLRExpansion","place-object-test");
+export const TLREXPANSION_GMTEST_PLACE_HS_TABLE_HUMAN : uint32 = GetID("Spell","TLRExpansion","gmtest-place-hs-table-human");
+export const TLREXPANSION_GMTEST_PLACE_HS_BOTTLE_GREEN : uint32 = GetID("Spell","TLRExpansion","gmtest-place-hs-bottle-green");
